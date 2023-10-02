46589f5e56f62b31db836a4e31e16113a1cafb63
BlantaGoose <sota.arai.s6@dc.tohoku.ac.jp>
2023-10-02 12:05:25 +0900

    Second commit

--- iris.R
+++ iris.R
@@ -1,2 +1,10 @@
 ##これはgitとRstudioの連携のチュートリアルです
+##参考ページhttps://qiita.com/zakkiiii/items/2ef92c8dd5d174a9ceed
 iris
+
+##右上ペーンの"Git"を開いてみると、?マークが２個並んでいる。
+##左側がGit、右側がディレクトリ
+##Let's Commit!!
+
+##スクリプトを変更しよう
+##まさにこの追記が変更になっているけど
